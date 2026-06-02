7e115d0c40272a2c40281dd02e85e10e3f9d2ede
harshitsinghbhandari565-del <harshitsinghbhandari565@gmail.com>
2026-06-02 16:06:36 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  compress: true,
+  poweredByHeader: false,
+}
+
+module.exports = nextConfig 
